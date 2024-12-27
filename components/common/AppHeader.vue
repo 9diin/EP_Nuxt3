c51daeb92a76b50e6a2933069bb7c140005bf17a
9diin <weartstudio@naver.com>
2024-12-27 19:33:06 +0900

    🔥 [init] 메인 페이지 소개 섹션 UI 완성

--- components/common/AppHeader.vue
+++ components/common/AppHeader.vue
@@ -1,8 +1,18 @@
 <template>
-    <header class="header"></header>
+    <header class="header">
+        <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">Nuxt3 News API App</h3>
+        <div class="flex items-center">
+            <Avatar class="cursor-pointer">
+                <AvatarImage src="https://github.com/9diin.png" alt="profile-image" />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+        </div>
+    </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+</script>
 
 <style scoped>
 .header {
@@ -10,5 +20,10 @@
 
     display: flex;
     align-items: center;
+    justify-content: space-between;
+
+    padding: 8px 24px;
+
+    border-bottom: 1px solid #f5f5f5;
 }
 </style>
