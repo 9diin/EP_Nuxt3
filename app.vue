9aa49d2d6bb20d443b52ad98ab2dc37652c7b204
9diin <weartstudio@naver.com>
2024-12-27 19:03:58 +0900

    🔥 [init] Nuxt3 프로젝트 생성 + Shadcn UI 패키지 설치

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app">
+        <AppHeader />
+        <NuxtPage />
+    </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from "./components/common/AppHeader.vue";
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-items: flex-start;
+}
+</style>
---
+++ components/common/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="header"></header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header {
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+}
+</style>
