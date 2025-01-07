4d0dbf5cdf65e103a60f826fa5cdf50abd0716f6
9diin <weartstudio@naver.com>
2025-01-07 11:55:02 +0900

    🔥 [feat] Pinia로 News API 호출하기

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,9 @@
 <template>
     <div class="page">
         <!-- 애플리케이션 소개 문구(텍스트) 레이아웃 -->
-        <div class="w-full flex flex-col items-center py-8 px-6 rounded-lg bg-neutral-100">
-            <p class="leading-7 font-light">WELCOME TO NUXT3 APPLICATION</p>
-            <div class="flex items-center gap-1 mt-3">
-                <h4 class="scroll-m-20 text-2xl font-semibold tracking-tight">Craft narratives ✍🏻 that ignite</h4>
-                <h4 class="scroll-m-20 text-2xl font-semibold tracking-tight text-red-500">inspiration💡</h4>
-            </div>
-            <div class="flex items-center gap-1">
-                <h4 class="scroll-m-20 text-2xl font-semibold tracking-tight text-red-500">knowledge 📘</h4>
-                <h4 class="scroll-m-20 text-2xl font-semibold tracking-tight">and</h4>
-                <h4 class="scroll-m-20 text-2xl font-semibold tracking-tight text-red-500">entertainment 🎬</h4>
-            </div>
-        </div>
+        <AppBanner />
         <!-- 메인 콘텐츠 레이아웃 -->
-        <div class="w-full flex items-center gap-6">
-            <Skeleton class="w-1/3 h-full rounded-lg" />
-            <div class="w-2/3 flex flex-col gap-6">
-                <div class="flex items-center gap-2">
-                    <Skeleton class="h-10 w-10 rounded-full" />
-                    <Skeleton class="w-20 h-5 rounded-lg" />
-                    &middot;
-                    <Skeleton class="w-32 h-5 rounded-lg" />
-                </div>
-                <div class="w-full flex flex-col gap-2">
-                    <Skeleton class="w-full h-10 rounded-lg" />
-                    <Skeleton class="w-full h-10 rounded-lg" />
-                </div>
-                <div class="w-full flex flex-col gap-2">
-                    <Skeleton class="w-full h-5 rounded-lg" />
-                    <Skeleton class="w-full h-5 rounded-lg" />
-                    <Skeleton class="w-full h-5 rounded-lg" />
-                </div>
-                <div class="flex items-center gap-2">
-                    <Skeleton class="w-20 h-5 rounded-lg" />
-                    &middot;
-                    <Skeleton class="w-20 h-5 rounded-lg" />
-                </div>
-            </div>
-        </div>
+        <SkeletonMainContent />
         <Separator label="Or" class="my-6" />
         <!-- 최근 콘텐츠 레이아웃 -->
         <div class="w-full flex flex-col gap-6">
@@ -50,63 +15,29 @@
                 </Button>
             </div>
             <div class="w-full grid grid-cols-4 gap-6">
-                <div class="w-full flex flex-col gap-4">
-                    <Skeleton class="w-full h-44 rounded-lg" />
-                    <div class="w-full flex flex-col gap-4">
-                        <div class="flex items-center gap-2">
-                            <Skeleton class="w-8 h-8 rounded-full" />
-                            <Skeleton class="w-16 h-4 rounded-lg" />
-                            &middot;
-                            <Skeleton class="w-20 h-4 rounded-lg" />
-                        </div>
-                        <div class="w-full flex flex-col gap-2">
-                            <Skeleton class="w-full h-6 rounded-lg" />
-                            <Skeleton class="w-full h-6 rounded-lg" />
-                        </div>
-                        <div class="w-full flex flex-col gap-2">
-                            <Skeleton class="w-full h-4 rounded-lg" />
-                            <Skeleton class="w-full h-4 rounded-lg" />
-                            <Skeleton class="w-full h-4 rounded-lg" />
-                        </div>
-                        <div class="flex items-center gap-2">
-                            <Skeleton class="w-16 h-4 rounded-lg" />
-                            &middot;
-                            <Skeleton class="w-20 h-4 rounded-lg" />
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full flex flex-col gap-4">
-                    <Skeleton class="w-full h-44 rounded-lg" />
-                    <div class="w-full flex flex-col gap-4">
-                        <div class="flex items-center gap-2">
-                            <Skeleton class="w-8 h-8 rounded-full" />
-                            <Skeleton class="w-16 h-4 rounded-lg" />
-                            &middot;
-                            <Skeleton class="w-20 h-4 rounded-lg" />
-                        </div>
-                        <div class="w-full flex flex-col gap-2">
-                            <Skeleton class="w-full h-6 rounded-lg" />
-                            <Skeleton class="w-full h-6 rounded-lg" />
-                        </div>
-                        <div class="w-full flex flex-col gap-2">
-                            <Skeleton class="w-full h-4 rounded-lg" />
-                            <Skeleton class="w-full h-4 rounded-lg" />
-                            <Skeleton class="w-full h-4 rounded-lg" />
-                        </div>
-                        <div class="flex items-center gap-2">
-                            <Skeleton class="w-16 h-4 rounded-lg" />
-                            &middot;
-                            <Skeleton class="w-20 h-4 rounded-lg" />
-                        </div>
-                    </div>
-                </div>
+                <SkeletonSubContent />
+                <SkeletonSubContent />
+                <SkeletonSubContent />
+                <SkeletonSubContent />
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { Button } from "~/components/ui/button";
+import { useStore } from "@/stores";
+/** 커스텀 컴포넌트 */
+import AppBanner from "~/components/common/AppBanner.vue";
+import SkeletonMainContent from "~/components/common/skeleton/SkeletonMainContent.vue";
+import SkeletonSubContent from "~/components/common/skeleton/SkeletonSubContent.vue";
+/** Shadcn-vue 컴포넌트 */
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { ArrowRight } from "lucide-vue-next";
+
+const store = useStore();
+
+onMounted(() => {
+    store.getNews();
+});
 </script>
--- stores/index.ts
+++ stores/index.ts
@@ -5,8 +5,9 @@ import axios from "axios";
 export const useStore = defineStore("store", () => {
     // State
     const searchValue = ref<string>("korea");
-    const mainArticle = ref<Article>();
+    const mainArticle = ref<Article | null>(null);
     const subArticles = ref<Article[]>([]);
+    const noDataMessage = ref<string | null>(null);
 
     // Actions
     // Mutations => State를 변경할 목적으로 작성된 코드
@@ -24,11 +25,18 @@ export const useStore = defineStore("store", () => {
 
             console.log(articles);
 
-            // 첫 번째 기사를 mainArticle에 설정
-            mainArticle.value = articles[0] || null;
+            if (articles.length > 0) {
+                // 첫 번째 기사를 mainArticle에 설정
+                mainArticle.value = articles[0] || null;
 
-            // 나머지 기사를 subArticles에 설정
-            subArticles.value = articles.slice(1);
+                // 나머지 기사를 subArticles에 설정
+                subArticles.value = articles.slice(1);
+
+                noDataMessage.value = null; // 데이터가 있으면 메시지 초기화
+            } else {
+                // 기사가 없을 경우, noDataMessage 설정
+                noDataMessage.value = "We couldn't find any articles matching your search.";
+            }
         } catch (error) {
             throw error;
         }
